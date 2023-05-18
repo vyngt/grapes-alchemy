9bdb104a07b91ece2766cfa380c46b5fc11ecb80
TheVy <ntvy2k@gmail.com>
2023-05-18 09:57:44 +0700

    Component lifecycle, setup and cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,39 @@
 
 import "grapesjs/dist/css/grapes.min.css";
 import grapesjs from "grapesjs";
-import { useEffect } from "react";
+import { Editor } from "grapesjs";
+
+import { useEffect, useState, useRef } from "react";
 
 export default function Home() {
+  const [editor, set_editor] = useState<Editor | undefined>();
+  const block_ref = useRef<HTMLDivElement>(null);
+
+  const on_add_block = () => {
+    if (editor) {
+      editor.BlockManager.add("custom-block", {
+        label: "Custom Block",
+        content: {
+          tagName: "div",
+          draggable: true,
+          attributes: { "some-attribute": "some-value" },
+          components: [
+            {
+              tagName: "span",
+              content: "<b>Some static content</b>",
+            },
+            {
+              tagName: "div",
+              components: "<span>HTML at some point</span>",
+            },
+          ],
+        },
+      });
+    }
+  };
+
   useEffect(() => {
-    grapesjs.init({
+    const _editor = grapesjs.init({
       container: "#gjs",
       fromElement: true,
       // Size of the editor
@@ -20,24 +48,56 @@ export default function Home() {
         appendTo: "#block",
         blocks: [
           {
-            id: "section", // id is mandatory
-            label: "<b>Section</b>", // You can use HTML/SVG inside labels
+            id: "section",
+            label: "<b>Section</b>",
             attributes: { class: "gjs-block-section" },
             content: `<section>
           <h1>This is a simple title</h1>
           <div>This is just a Lorem text: Lorem ipsum dolor sit amet</div>
         </section>`,
           },
+          {
+            id: "text",
+            label: "Text",
+            content: '<div data-gjs-type="text">Insert your text here</div>',
+          },
+          {
+            id: "image",
+            label: "Image",
+            select: true,
+            content: { type: "image" },
+            activate: true,
+          },
         ],
       },
     });
+    set_editor(_editor);
+
+    return () => {
+      editor?.destroy();
+      if (block_ref) {
+        const current = block_ref.current;
+        if (current) {
+          console.log("Destroyed");
+          current.innerHTML = "";
+        }
+      }
+      set_editor(undefined);
+    };
   }, []);
 
   return (
     <main className="h-screen">
       <div className="min-h-full">
+        <button
+          onClick={() => {
+            on_add_block();
+          }}
+        >
+          Add Button
+        </button>
         <div id="gjs"></div>
-        <div id="block"></div>
+        <div id="block" ref={block_ref}></div>
       </div>
     </main>
   );
